2dd54018babb749959185b4eaaf772f49511beb4
Philipp Thölke <p.thoelke@gmx.de>
2021-07-23 14:28:24 +0200

    Better VMD visualization

--- scripts/attention_weights.py
+++ scripts/attention_weights.py
@@ -72,6 +72,8 @@ def extract_data(model_path, dataset_path, dataset_name, dataset_arg, batch_size
             for mol_idx in batch.batch.unique():
                 rollout_batch = batch.batch[attention_weights.rollout_index[-1][0]]
                 if plot_molecules == 'VMD':
+                    vmd = getCurrentViewer()
+
                     # visualize using VMD
                     mask = batch.batch == mol_idx
                     mol = Molecule().empty(mask.sum())
@@ -79,18 +81,32 @@ def extract_data(model_path, dataset_path, dataset_name, dataset_arg, batch_size
                     mol.element[:] = [num2elem[num] for num in batch.z[mask].numpy()]
                     mol.name[:] = [num2elem[num] for num in batch.z[mask].numpy()]
 
-                    max_attn = attention_weights.rollout_weights[-1][rollout_batch == mol_idx].max()
+                    max_attn = attention_weights.rollout_weights[-1][rollout_batch == mol_idx].abs().max()
                     for idx1, idx2 in attention_weights.rollout_index[-1].T[rollout_batch == mol_idx]:
                         attn_idx = torch.where((attention_weights.rollout_index[-1][0] == idx1) & (attention_weights.rollout_index[-1][1] == idx2))[0]
-                        radius = float(attention_weights.rollout_weights[-1][attn_idx] / max_attn)
-                        if radius > 0.25:
-                            VMDCylinder(mol.coords[idx1 - idx_offset].flatten(), mol.coords[idx2 - idx_offset].flatten(), color='red', radius=0.05 * radius)
-                        elif radius < 0:
-                            VMDCylinder(mol.coords[idx1 - idx_offset].flatten(), mol.coords[idx2 - idx_offset].flatten(), color='blue', radius=0.05 * radius)
+                        strength = float(attention_weights.rollout_weights[-1][attn_idx] / max_attn)
+                        
+                        draw_thresholds = (0.1, -0.1)
+                        if strength > draw_thresholds[0] or strength < draw_thresholds[1]:
+                            vmd.send(f'mol new')
+                            vmd.send(f'material add mat-{idx1}-{idx2}')
+                            vmd.send(f'material change opacity mat-{idx1}-{idx2} {abs(strength) ** 2}')
+                            vmd.send(f'material change specular mat-{idx1}-{idx2} 0')
+                            vmd.send(f'draw material mat-{idx1}-{idx2}')
+
+                            if strength > draw_thresholds[0]:
+                                VMDCylinder(mol.coords[idx1 - idx_offset].flatten(), mol.coords[idx2 - idx_offset].flatten(), color='red', radius=0.05 * abs(strength))
+                            elif strength < draw_thresholds[1]:
+                                VMDCylinder(mol.coords[idx1 - idx_offset].flatten(), mol.coords[idx2 - idx_offset].flatten(), color='blue', radius=0.05 * abs(strength))
                     
-                    viewer = getCurrentViewer()
-                    mol.view(style='CPK', viewerhandle=viewer)
-                    while not viewer.completed():
+                    mol.view(style='Licorice', viewerhandle=vmd)
+                    vmd.send('mol modstyle 0 top Licorice 0.1 12 12')
+                    vmd.send('mol modmaterial 0 top Transparent')
+                    vmd.send(f'material change opacity Transparent 0.25')
+                    vmd.send(f'material change opacity Diffuse 1')
+                    vmd.send(f'material change opacity Specular 0')
+                    vmd.send('display rendermode GLSL')
+                    while not vmd.completed():
                         time.sleep(0.1)
                 elif plot_molecules == 'matplotlib':
                     # visualize using matplotlib
