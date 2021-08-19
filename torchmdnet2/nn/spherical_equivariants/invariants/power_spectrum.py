7998b2e8762b3f963173bf861344fdb9c2487aca
felixmusil <felix.musil@epfl.ch>
2021-08-19 16:23:35 +0200

    adapt powerspectrum to leading l dimension

--- torchmdnet2/nn/spherical_equivariants/invariants/power_spectrum.py
+++ torchmdnet2/nn/spherical_equivariants/invariants/power_spectrum.py
@@ -1,3 +1,5 @@
+from mpmath.libmp import backend
+from numpy.lib.twodim_base import triu_indices
 import torch
 import torch.nn as nn
 import math
@@ -16,7 +18,7 @@ def powerspectrum(se_, nsp, nmax, lmax):
                                    dim=5) / math.sqrt(lbs)
         idx += lbs
     ps = ps.view(J, nsp* nmax, nsp* nmax, lmax)
-    PS = torch.zeros(J, int((nsp* nmax+1)**2/2), lmax, dtype=se.dtype, device=se.device)
+    PS = torch.zeros(J, int(nsp* nmax*(nsp* nmax+1)/2), lmax, dtype=se.dtype, device=se.device)
 
     fac = math.sqrt(2.) * torch.ones((nsp*nmax,nsp*nmax))
     fac[range(nsp*nmax), range(nsp*nmax)] = 1.
@@ -31,24 +33,41 @@ def powerspectrum_(se_, nsp, nmax, lmax):
     dtype = se_[0].dtype
     device = se_[0].device
 
-    se = []
+    PS = torch.zeros(J, lmax, int(nsp* nmax*(nsp* nmax+1)/2), dtype=dtype, device=device)
+    diag_ids = torch.arange(nsp* nmax, dtype=torch.long, device=device)
+    upper_tri = torch.triu_indices(nsp * nmax, nsp * nmax, offset=1, dtype=torch.long, device=device)
+    # triu_ids = torch.triu_indices(nsp * nmax, nsp * nmax, offset=0, dtype=torch.long, device=device)
+    # diag_ids = torch.tensor([0] + [nsp * nmax-ii for ii in range(nsp * nmax-1)], dtype=torch.long)
+    # diag_ids = torch.cumsum(diag_ids, dim=0)
+    # off_diag_mask = torch.ones(triu_ids.shape[1],dtype=bool)
+    # off_diag_mask[diag_ids] = False
     for l in range(lmax):
-        se.append(se_[l].view(J, nsp* nmax, 2*l+1))
+        se = se_[l].view(J, nsp* nmax, 2*l+1)
+        PS[:, l, :nsp * nmax] = oe.contract('iam,iam->ia', se[:, diag_ids, :], se[:, diag_ids, :], backend='torch') / math.sqrt(2*l+1)
+        # for ii in range(nsp*nmax):
+        #     PS[l, :, ii] = torch.sum(se[:, ii, :]* se[:, ii, :], dim=-1)
+        PS[:, l, nsp * nmax:] = math.sqrt(2.) * oe.contract(
+            'iam,iam->ia', se[:, upper_tri[0], :], se[:, upper_tri[1], :], backend='torch') / math.sqrt(2*l+1)
+        # PS[:, l] = oe.contract(
+        #     'iam,iam->ia', se[:, triu_ids[0], :], se[:, triu_ids[1], :], backend='torch') / math.sqrt(2*l+1)
+        # PS[:, l, off_diag_mask] = PS[:, l, off_diag_mask].mul_(math.sqrt(2.))
 
-    ps = torch.zeros(lmax, J, nsp* nmax, nsp* nmax, dtype=dtype, device=device)
-    for l in range(lmax):
-        ps[l] = oe.contract('iam,ibm->iab', se[l], se[l]) / math.sqrt(2*l+1)
-
-    # ps = ps.view(lmax, J, nsp* nmax, nsp* nmax)
-    PS = torch.zeros(J, lmax, int((nsp* nmax+1)**2/2), dtype=dtype, device=device)
-
-    fac = math.sqrt(2.) * torch.ones((nsp*nmax,nsp*nmax))
-    fac[range(nsp*nmax), range(nsp*nmax)] = 1.
-    ids = [(i,j) for i in range(nsp*nmax)
-                for j in range(nsp*nmax) if j >= i]
-    for ii, (i, j) in enumerate(ids):
-            PS[:, :, ii] = fac[i,j]*ps[:,:, i, j].transpose(0,1)
     return PS.view(J, -1)
+    # ps = torch.zeros(lmax, J, nsp* nmax, nsp* nmax, dtype=dtype, device=device)
+    # for l in range(lmax):
+    #     se = se_[l].view(J, nsp* nmax, 2*l+1)
+    #     ps[l] = oe.contract('iam,ibm->iab', se, se) / math.sqrt(2*l+1)
+
+    # # ps = ps.view(lmax, J, nsp* nmax, nsp* nmax)
+    # PS = torch.zeros(J, lmax, int(nsp* nmax*(nsp* nmax+1)/2), dtype=dtype, device=device)
+
+    # fac = math.sqrt(2.) * torch.ones((nsp*nmax,nsp*nmax))
+    # fac[range(nsp*nmax), range(nsp*nmax)] = 1.
+    # ids = [(i,j) for i in range(nsp*nmax)
+    #             for j in range(nsp*nmax) if j >= i]
+    # for ii, (i, j) in enumerate(ids):
+    #         PS[:, :, ii] = fac[i,j]*ps[:,:, i, j].transpose(0,1)
+    # return PS.view(J, -1)
 
 class PowerSpectrum(nn.Module):
     def __init__(self, max_radial: int, max_angular: int,
@@ -71,10 +90,10 @@ class PowerSpectrum(nn.Module):
 
         self.se = SphericalExpansion(max_radial, max_angular, interaction_cutoff, gaussian_sigma_constant, species, smooth_width=smooth_width)
 
-        self.D = int((self.n_species*self.nmax+1)**2/2) * (self.lmax+1)
+        self.D = int(self.n_species*self.nmax*(self.n_species*self.nmax+1)/2) * (self.lmax+1)
 
     def size(self):
-        return int((self.n_species*self.nmax+1)**2/2) * (self.lmax+1)
+        return int(self.n_species*self.nmax*(self.n_species*self.nmax+1)/2) * (self.lmax+1)
 
     def forward(self, data):
         # ci_anlm = self.se(data)
