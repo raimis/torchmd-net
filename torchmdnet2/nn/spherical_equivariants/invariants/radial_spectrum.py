333749ec7385220953e3823c9ed72406ffee038d
felixmusil <felix.musil@epfl.ch>
2021-08-26 16:16:25 +0200

    fix radial spectrum missing arguement and method

--- torchmdnet2/nn/spherical_equivariants/invariants/radial_spectrum.py
+++ torchmdnet2/nn/spherical_equivariants/invariants/radial_spectrum.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
+from typing import List, Union
 
 from ..spherical_expansion import SphericalExpansion
 
 
 class RadialSpectrum(nn.Module):
-    def __init__(self, max_radial, interaction_cutoff,
-                                gaussian_sigma_constant, species, normalize=True):
+    def __init__(self, max_radial: int, interaction_cutoff: float,
+                                gaussian_sigma_constant: float, species: Union[List[int], torch.Tensor], normalize: bool =True, smooth_width: float=0.5):
         super(RadialSpectrum, self).__init__()
         self.nmax = max_radial
         self.lmax = 0
@@ -14,15 +15,20 @@ class RadialSpectrum(nn.Module):
         self.sigma = gaussian_sigma_constant
         self.normalize = normalize
 
+        if isinstance(species, list):
+            species = torch.tensor(species, dtype=torch.long)
         self.species, _ = torch.sort(species)
         self.n_species = len(species)
         self.species2idx = -1*torch.ones(torch.max(species)+1,dtype=torch.int32)
         for isp, sp in enumerate(self.species):
             self.species2idx[sp] = isp
 
-        self.se = SphericalExpansion(max_radial, 0, interaction_cutoff, gaussian_sigma_constant, species)
+        self.se = SphericalExpansion(max_radial, 0, interaction_cutoff, gaussian_sigma_constant, species, smooth_width)
 
         self.D = self.n_species*self.nmax
+
+    def size(self):
+        return int(self.n_species*self.nmax)
 
     def forward(self, data):
         ci_an = self.se(data)[0]
