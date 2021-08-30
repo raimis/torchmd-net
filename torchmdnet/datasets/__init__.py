d7d5ceb201e1f2ee5a8e9e261c95bbf0b8ff4c5c
Philipp Thölke <p.thoelke@gmx.de>
2021-08-30 21:58:23 +0200

    Implement QM9 dataset using old code base

--- torchmdnet/datasets/__init__.py
+++ torchmdnet/datasets/__init__.py
@@ -3,5 +3,6 @@ from .md17 import MD17
 from .ani1 import ANI1
 from .custom import Custom
 from .hdf import HDF5
+from .qm9_old import QM9_old
 
-__all__ = ["QM9", "MD17", "ANI1", "Custom", "HDF5"]
+__all__ = ["QM9", "MD17", "ANI1", "Custom", "HDF5", "QM9_old"]
