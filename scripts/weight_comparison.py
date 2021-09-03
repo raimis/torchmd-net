ae7ecf2ca23905e751a3adc4609a437ae05754ec
Philipp Thölke <p.thoelke@gmx.de>
2021-09-03 12:12:48 +0200

    Adjust plot labels

--- scripts/weight_comparison.py
+++ scripts/weight_comparison.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 
 zoom = 1.5
 num_mols = 3
-visualize = True
+visualize = False
 img_dir = "/home/philipp/Desktop/imgs/"
 
 model_paths = [
@@ -18,9 +18,9 @@ model_paths = [
     "/home/philipp/Documents/models/MD17-uracil-et/epoch*",
     "/home/philipp/Documents/models/ANI1-energy-et/epoch*",
 ]
-dataset_name = "MD17"
+dataset_name = "QM9"
 
-dset2name = {"ANI1": "ANI-1", "QM9": "QM9", "MD17": "MD17"}
+dset2name = {"ANI1": "ANI-1", "QM9": "QM9", "MD17": "MD17 (_mol_)"}
 dset2splits = {
     "QM9": "/home/philipp/Documents/models/QM9-energy_U0-et/splits.npz",
     "MD17": "/home/philipp/Documents/models/MD17-uracil-et/splits.npz",
@@ -76,15 +76,20 @@ fig, axes = plt.subplots(
     num_mols + 1,
     gridspec_kw=dict(width_ratios=[0.1] + [1] * num_mols, wspace=0, hspace=0),
 )
-fig.suptitle(f"Molecules from {dset2name[dataset_name]}", fontsize=title_size)
-fig.text(0.08, 0.5, "trained on", va="center", rotation="vertical", fontsize=title_size)
+fig.suptitle(
+    f"Molecules from {dset2name[dataset_name].replace('_mol_', 'aspirin')}",
+    fontsize=title_size,
+)
+fig.text(
+    0.08, 0.5, "model trained on", va="center", rotation="vertical", fontsize=title_size
+)
 
 for i, model_name in enumerate(images.keys()):
     axes[i, 0].axis("off")
     axes[i, 0].text(
         0.5,
         0.5,
-        dset2name[model_name.split("_")[0]],
+        dset2name[model_name.split("_")[0]].replace("_mol_", "uracil"),
         rotation=90,
         ha="center",
         va="center",
@@ -94,7 +99,7 @@ for i, model_name in enumerate(images.keys()):
     for mol_idx in range(num_mols):
         if i == 0:
             axes[i, mol_idx + 1].set_title(
-                f"Molecule {mol_idx + 1}", fontsize=text_size
+                f"molecule {mol_idx + 1}", fontsize=text_size
             )
         axes[i, mol_idx + 1].imshow(images[model_name][mol_idx])
         axes[i, mol_idx + 1].axis("off")
