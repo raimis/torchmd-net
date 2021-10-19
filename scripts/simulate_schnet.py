ff2c9888bd9e237f7526571b5bd315579d407491
felixmusil <felix.musil@epfl.ch>
2021-10-19 17:45:51 +0200

    aa

--- scripts/simulate_schnet.py
+++ scripts/simulate_schnet.py
@@ -25,76 +25,53 @@ from pytorch_lightning.plugins import DDPPlugin
 from torch_geometric.data import DataLoader
 
 from torch.nn import Embedding, Sequential, Linear, ModuleList
-
-from os.path import join
 from time import ctime
-
-# def get_args():
-#     # fmt: off
-#     parser = argparse.ArgumentParser(description='Simulate')
-#     parser.add_argument('--dataset_root', type=str, help='root path to the dataset')
-#     parser.add_argument('--checkpoint_fn', type=str, help='path to the checkpoint to load the model from')
-#     parser.add_argument('--device', default='cuda' type=str, help='specify the device to run the simulation on')
-#     parser.add_argument('--model', )
-
-#     args = parser.parse_args()
-
-#     save_argparse(args, os.path.join(args.log_dir, 'simulation_input.yaml'), exclude=['conf'])
-
-#     return args
+from os.path import join
 
 if __name__ == "__main__":
-    device = torch.device('cuda:3')
+    device = torch.device('cuda:2')
     print(f'Simulation starts on: {ctime()}')
 
-    # basefn = '/local_scratch/musil/chign/soap_mlp/rs_diff_species_0'
-    # prefix = 'ep99_dt5'
-    # ckpt = 'epoch=99-validation_loss=27.2705-test_loss=0.0000.ckpt'
-    # basefn = '/local_scratch/musil/chign/soap_mlp/diff_species_1'
-    # prefix = 'traj-ep_41'
-    basefn = '/local_scratch/musil/chign/soap_mlp/diff_species_2/'
-    ckpt_id = 49
-    ckpt = {
-        9 : 'epoch=9-validation_loss=27.2639-test_loss=0.0000.ckpt',
-        19 : 'epoch=19-validation_loss=27.2561-test_loss=0.0000.ckpt',
-        29 : 'epoch=29-validation_loss=27.2540-test_loss=0.0000.ckpt',
-        49 : 'epoch=49-validation_loss=27.2462-test_loss=0.0000.ckpt'
-    }
+    n_sims = 500
+    simulation_time = 5e3 # ps
 
-    n_sims = 400
-    simulation_time = 2e3 # ps
-
-    save_interval = 20
+    save_interval = 10
     export_interval = 100000
     log_interval = 100000
     dt = 0.005 # ps
-    friction = 5 # ps ^-1
+    friction = 1 # ps ^-1
 
-    n_timesteps = int(simulation_time / dt)
-    print(f'of {n_sims} trajectories for {simulation_time} [ps] ({n_timesteps} timesteps)')
 
     temperature = 350 # K
     k_b = 0.001985875 #   kcal/mol / K
     beta = 1 / (k_b * temperature) #   kcal/mol ^-1
-
     masses = np.array([
             12, 12, 12, 12, 12,
             12, 12, 12, 12, 12,
         ])
     mass_scale = 418.4 #
 
-    prefix = f'traj-e_{ckpt_id:d}-dt_{dt*1000:.0f}fs'
-    outfn = join(basefn,prefix,'traj')
-    if not os.path.exists(join(basefn,prefix)):
-        print(f'Making {join(basefn,prefix)}')
-        os.mkdir(join(basefn,prefix))
+    ckpt = {
+        49 : 'epoch=49-validation_loss=739.7512-test_loss=0.0000.ckpt',
+        25 : 'epoch=25-validation_loss=740.0948-test_loss=0.0000.ckpt',
+        15 : 'epoch=15-validation_loss=740.3742-test_loss=0.0000.ckpt',
+    }
+
+
+    basefn = '/local_scratch/musil/chign/schnet/test_1/'
 
     chignolin_dataset = ChignolinDataset('/local_scratch/musil/datasets/chignolin/')
 
+    n_timesteps = int(simulation_time / dt)
+    print(f'of {n_sims} trajectories for {simulation_time} [ps] ({n_timesteps} timesteps)')
+
     baseline_model = chignolin_dataset.get_baseline_model(n_beads=10)  # doesn't work without specifying n_beads
 
-    model = MLPModel.load_from_checkpoint(
-        join(basefn, ckpt[ckpt_id])
+
+    prefix = f'traj-e_{ckpt[15]}-dt_{dt*1000:d0}fs'
+
+    model = SchNet.load_from_checkpoint(
+        join(basefn, ckpt[15])
     )
 
 
@@ -116,10 +93,26 @@ if __name__ == "__main__":
                     save_potential=True, device=device,
                     log_interval=log_interval, log_type='print',
                     batch_size=600, export_interval=export_interval,
-                    filename=outfn,
+                    filename=join(basefn,prefix)
     )
 
 
     traj = sim.simulate()
 
     print(f'Simulation ended on: {ctime()}')
+    # np.save('/local_scratch/musil/chign/test_4/traj.npy', traj)
+    # with torch.profiler.profile(
+    #     on_trace_ready=torch.profiler.tensorboard_trace_handler('/local_scratch/musil/chign/test_5'),
+    #     record_shapes=False,
+    #     with_stack=True
+    #     ) as prof:
+
+    #     traj = sim.simulate()
+
+    # torch.save(traj, '/local_scratch/musil/chign/traj.pt')
+
+    # fig,_, tica = plot_tica(baseline_model, chignolin_dataset, lag=10)
+    # plt.savefig('/local_scratch/musil/chign/ref_traj.png', dpi=300, bbox_inches='tight')
+
+    # fig,_,_ = plot_tica(baseline_model, traj, tica=tica)
+    # plt.savefig('/local_scratch/musil/chign/simulated_traj.png', dpi=300, bbox_inches='tight')
